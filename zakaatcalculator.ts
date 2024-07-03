6706c0067668e5067e847dcd15c4fceaa306ebdb
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-07-03 12:15:26 -0700

    Add files via upload
     zakaat-calculator using by inquirer & npx

---
+++ zakaatcalculator.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer"
+
+let zakat:number =2.5;
+let goldValueInTola =234800;
+let goldWeightInTolaReq =7.5;
+
+
+let isConditionTrue = true;
+while(isConditionTrue){
+    const answer =await inquirer.prompt([
+        {
+            name:"goldWeight",
+            massage:"Enter goldWeight:",
+            type:"number"
+    
+    
+    }]);
+    
+    let goldWeight = answer.goldWeight;
+     if (goldWeight >= goldWeightInTolaReq) {
+        let calculatedzakat =(goldWeight * goldValueInTola) * (zakat/100);
+        console.log( `calculated value of gold weighing
+         ${goldWeight} tola is ${calculatedzakat}`);
+     }else{
+        console.log("zakat not applicable");
+        break //or condition =false;
+     }
+    
+}
+const answer =await inquirer.prompt([
+    {
+        name:"goldWeight",
+        massage:"Enter goldWeight:",
+        type:"number"
+
+
+}]);
+
+let goldWeight = answer.goldWeight;
+ if (goldWeight >= goldWeightInTolaReq) {
+    let calculatedzakat =(goldWeight * goldValueInTola) * (zakat/100);
+    console.log( `calculated value of gold weighing
+     ${goldWeight} tola is ${calculatedzakat}`);
+ }else{
+    console.log("zakat not applicable");
+ }
